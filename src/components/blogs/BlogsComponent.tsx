dae41775d80b6a572eace2643cd0e74776fce080
Saidbek <sherzodraxmonaliev7@gmail.com>
2024-03-27 14:52:37 +0500

    add

--- src/components/blogs/BlogsComponent.tsx
+++ src/components/blogs/BlogsComponent.tsx
@@ -17,20 +17,19 @@ const BlogsComponent = () => {
     <div className={styles.blogs}>
       <h2>{t("Payment.12")}</h2>
       <div className={styles.blogs_boxs}>
-        {data?.blogss?.map((elem:BlogsBoxType, index:number) => {
-       return (
+        {data?.blogss?.map((elem:BlogsBoxType, index:number) => (
           <div key={index} className={styles.blogs_card}>
             <Image src={elem?.image.url} width={683} height={307} alt="Blog Image" />
             <div>
             <h3>{locale == 'ru' ? elem?.titleRu : locale == 'en' ? elem?.titleEn :locale == 'uz' ? elem?.titleUz :elem?.titleRu}</h3>
-            <p style={{whiteSpace : "pre-wrap"}}>{locale == 'ru' ? elem?.descriptionRu.text?.slice(0  ,300) : locale == 'en' ? elem?.descriptionEn.text?.slice(0  ,300) :  locale == 'uz' ? elem?.descriptionUz.text?.slice(0  ,300) : elem?.descriptionRu.text?.slice(0  ,300) }</p>
+            <p>{locale == 'ru' ? elem?.descriptionRu.text?.slice(0  ,300) : locale == 'en' ? elem?.descriptionEn.text?.slice(0  ,300) :  locale == 'uz' ? elem?.descriptionUz.text?.slice(0  ,300) : elem?.descriptionRu.text?.slice(0  ,300) }</p>
             <div className={styles.date}>
                 <span className={styles.date_text}>{elem?.date}</span>
                 <span className={styles.data_button} onClick={()=>router.push(`/blog/${elem?.slug}`)}>{t('Payment.13')} <PlayArrowIcon sx={{color:'rgba(226, 49, 54, 1)' , cursor:'pointer'}}/></span>
             </div>
             </div>
           </div>
-        )})}
+        ))}
       </div>
     </div>
   );
